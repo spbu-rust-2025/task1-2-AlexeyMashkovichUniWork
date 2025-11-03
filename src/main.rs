2b190e4922bbb0d5b53a54b5b47da67cf5e2ec72
AlexeyMashkovichUniWork <st142037@student.spbu.ru>
2025-11-03 14:35:30 +0300

    Get line-by-line data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
+use std::io;
 
+fn main() -> io::Result<()> {
+	let mut buffer : String;
+	let stdin = io::stdin();
+	loop {
+		buffer = "".to_string();
+		stdin.read_line(&mut buffer)?;
+		let parsed_buffer = buffer.trim();
+		println!("{parsed_buffer}");
+		if parsed_buffer == "-1" {
+			break;
+		}
+	}
+	Ok(())
 }
