53e961167a440e1e4b868a302f187ff402f3e088
AlexeyMashkovichUniWork <st142037@student.spbu.ru>
2025-11-03 14:50:24 +0300

    add addition

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
 use std::io;
 
 fn main() -> io::Result<()> {
-	let mut buffer : String;
-	let stdin = io::stdin();
-	loop {
-		buffer = "".to_string();
-		stdin.read_line(&mut buffer)?;
-		let parsed_buffer = buffer.trim();
-		println!("{parsed_buffer}");
-		if parsed_buffer == "-1" {
-			break;
-		}
-	}
-	Ok(())
+    let mut buffer: String;
+    let stdin = io::stdin();
+    let mut sum: u128 = 0;
+    loop {
+        buffer = "".to_string();
+        stdin.read_line(&mut buffer)?;
+        let parsed_buffer = buffer.trim();
+        if parsed_buffer == "-1" {
+            println!("{sum}");
+            break;
+        }
+        match parsed_buffer.parse::<u128>() {
+            Ok(number) => sum += number,
+            Err(_) => {
+                println!("NaN");
+                break;
+            }
+        }
+    }
+    Ok(())
 }
